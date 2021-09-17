ab524893c156fce7024e84fd2a7316439ae30068
gilmarferrini <ferrinigilmar@gmail.com>
2021-09-17 20:47:08 -0300

    adicionado rota para cadastrar um usuario

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
-const express = require("express")
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
-const app = express()
+const app = express();
 
-app.listen(3333)
+app.use(express.json());
+
+const customers = [];
+
+/*
+  * cpf - string
+  * name - string
+  * id - uuid
+  * statement []
+*/
+app.post("/accounts", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const id = uuidv4();
+
+  customers.push({
+    id,
+    cpf,
+    name,
+    statement: []
+  });
+
+  return response.status(201).json(customers);
+});
+
+app.listen(3333);
