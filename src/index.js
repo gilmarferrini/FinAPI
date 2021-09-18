2f022e48603bce113cefd16cc3853640a0f4e162
gilmarferrini <ferrinigilmar@gmail.com>
2021-09-18 10:42:58 -0300

    adicionado rota de saque

--- src/index.js
+++ src/index.js
@@ -22,6 +22,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 /*
   * cpf - string
   * name - string
@@ -72,6 +84,27 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
 
   return response.status(201).send()
 
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds!" });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit"
+  }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(200).send();
 });
 
 app.listen(3333);
